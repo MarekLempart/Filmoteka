a6ef6a682527b4831cdf2531098870eb0f47f900
MarekLempart <kama.zakupy@gmail.com>
2024-03-08 20:13:26 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,53 +1,53 @@
 // index.js
 
-// import axios from 'axios';
-// import { getMoviesByKeyword } from './api--ML.js';
-// import { renderMovies } from './renderFilms.js';
+import axios from 'axios';
+import { getMoviesByKeyword } from './api--ML.js';
+import { renderMovies } from './renderFilms.js';
 
 import renderGallery from './gallery.js'; // Importujemy funkcję renderGallery z pliku gallery.js
 
 // Wywołujemy funkcję renderGallery po załadowaniu strony
 window.addEventListener('DOMContentLoaded', renderGallery);
 
-// const genreInput = document.getElementById('genre-input');
-// const searchButton = document.getElementById('search-button');
+const genreInput = document.getElementById('genre-input');
+const searchButton = document.getElementById('search-button');
 
-// searchButton.addEventListener('click', async () => {
-//   const genre = genreInput.value; // Pobieramy value z pola input
-//   console.log('Searching for movies with genre:', genre);
-//   const movies = await getMoviesByKeyword(genre);
-//   console.log('Movies:', movies);
-//   renderMovies(movies);
-// });
+searchButton.addEventListener('click', async () => {
+  const genre = genreInput.value; // Pobieramy value z pola input
+  console.log('Searching for movies with genre:', genre);
+  const movies = await getMoviesByKeyword(genre);
+  console.log('Movies:', movies);
+  renderMovies(movies);
+});
 
-// document.addEventListener('click', event => {
-//   if (event.target.classList.contains('movie-image')) {
-//     const movieId = event.target.dataset.movieId;
-//     console.log('Clicked on movie with ID:', movieId);
-//     showModal(movieId);
-//   }
-// });
+document.addEventListener('click', event => {
+  if (event.target.classList.contains('movie-image')) {
+    const movieId = event.target.dataset.movieId;
+    console.log('Clicked on movie with ID:', movieId);
+    showModal(movieId);
+  }
+});
 
-// const showModal = async movieId => {
-//   try {
-//     const response = await axios.get(
-//       `https://api.themoviedb.org/3/movie/${movieId}?api_key=c94d8e5ef8b4fe69956b21ebd01a6f37`
-//     );
-//     const movie = response.data;
+const showModal = async movieId => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=c94d8e5ef8b4fe69956b21ebd01a6f37`
+    );
+    const movie = response.data;
 
-//     // Sprawdź, czy istnieje tytuł filmu, zanim spróbujesz go wyświetlić
-//     if (movie.title) {
-//       console.log('Movie title:', movie.title);
-//     } else {
-//       console.log('Movie title not available');
-//     }
+    // Sprawdź, czy istnieje tytuł filmu, zanim spróbujesz go wyświetlić
+    if (movie.title) {
+      console.log('Movie title:', movie.title);
+    } else {
+      console.log('Movie title not available');
+    }
 
-//     // Tutaj możesz zaimplementować wyświetlanie informacji o filmie w oknie modalnym
-//     console.log('Movie details:', movie);
+    // Tutaj możesz zaimplementować wyświetlanie informacji o filmie w oknie modalnym
+    console.log('Movie details:', movie);
 
-//     // Na przykład, możemy wyświetlić tytuł filmu w konsoli
-//     console.log('Movie title:', movie.title);
-//   } catch (error) {
-//     console.error('Error fetching movie details:', error);
-//   }
-// };
+    // Na przykład, możemy wyświetlić tytuł filmu w konsoli
+    console.log('Movie title:', movie.title);
+  } catch (error) {
+    console.error('Error fetching movie details:', error);
+  }
+};
